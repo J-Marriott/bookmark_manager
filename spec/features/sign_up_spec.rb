556d834d0b1e834043f7e4d05a314e7720cd0805
Joe Marriott <marriott90@gmail.com>
2017-01-25 13:47:28 +0000

    displays user email on welcome

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,9 +1,9 @@
 feature 'sign up to website' do
   scenario 'User can sign up to website' do
+    expect { sign_up }.to change(User, :count).by(1)
+    expect(page).to have_content 'bookmarks'
+    # expect(page).to have_content 'Welcome'
 
-    expect{ sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content "bookmarks"
-        expect(page).to have_content "Welcome"
-    # expect(page).to have_content "Welcome mememe@not_a_url.com"
+    expect(page).to have_content 'Welcome mememe@not_a_url.com'
   end
 end
